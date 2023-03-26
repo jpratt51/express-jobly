0b4fe57168e6d36a9a2917909c7a3d870fe4f1d9
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-03-26 09:26:49 -0500

    initial commit. Added documentation for sqlForPartialUpdate function in sql.js on lines 3-11.

---
+++ helpers/sql.js
@@ -0,0 +1,27 @@
+const { BadRequestError } = require('../expressError');
+
+/** Helpers: SQL for Update.
+ *
+ * Converts user input into sql for updating database:
+ * {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+ *
+ * Will accept provided fields even if not all fields are provided and only update * provided fields.
+ *
+ * If no input is sent, throws BadRequestError.
+ */
+
+function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+    const keys = Object.keys(dataToUpdate);
+    if (keys.length === 0) throw new BadRequestError('No data');
+
+    const cols = keys.map(
+        (colName, idx) => `"${jsToSql[colName] || colName}"=$${idx + 1}`
+    );
+
+    return {
+        setCols: cols.join(', '),
+        values: Object.values(dataToUpdate),
+    };
+}
+
+module.exports = { sqlForPartialUpdate };
