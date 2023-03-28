8e85e3fb50e414c18befc8b1f941b6c7f3c90230
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-03-28 00:41:53 -0500

    Created function ensureAdmin in middleware/auth.js to test whether logged in user is admin or not lines 44-52. Exported ensureAdmin function line 57 of middleware/auth.js. Wrote tests for ensureAdmin function in middleware/auth.test.js lines 76-95. Added user with Admin status to routes/_testCommon.js on lines 60-67. Created token for u4 on line 83 and exported token on line 91 of routes/_testCommon.js. Imported ensureAdmin function into routes/companies.js on line 9 and added ensureAdmin middleware to POST companies, PATCH companies/:handle and DELETE companies/:handle routes on lines 28, 110 and 138. Updated tests for routes/companies.test.js by importing u4Token on line 14, testing that only admin can create a company on lines 33-50, 59 and 70, testing that only admin can update a company on lines 181-207, 222, 232 and 242, and testing that only an admin can delete a company on lines 250-262 and 272. Imported ensureAdmin middleware into routes/users.js on line 8, implemented ensureAdmin middleware for POST users and GET users on lines 29 and 52. Added logic to ensure only admins or users with the same username can get details on a user on lines 69-77 of routes/users.js. Updated tests for routes/users.test.js by importing u4Token on line 15, testing that only admins and not non-admin users can create a new user on lines 26-89, 109 and 125. Updated tests for routes/users.test.js, testing that only admins and not non-admin users can get a list of all users on lines 132-175 and 189. Updated tests for routes/users.test.js, testing that only admins and same username users can get details for a user for GET /users/:username route on lines 197-232 and 242.

--- routes/companies.js
+++ routes/companies.js
@@ -6,7 +6,7 @@ const jsonschema = require('jsonschema');
 const express = require('express');
 
 const { BadRequestError } = require('../expressError');
-const { ensureLoggedIn } = require('../middleware/auth');
+const { ensureLoggedIn, ensureAdmin } = require('../middleware/auth');
 const Company = require('../models/company');
 const { parseQueries } = require('../helpers/validators');
 
@@ -25,7 +25,7 @@ const router = new express.Router();
  * Authorization required: login
  */
 
-router.post('/', ensureLoggedIn, async function (req, res, next) {
+router.post('/', ensureLoggedIn, ensureAdmin, async function (req, res, next) {
     try {
         const validator = jsonschema.validate(req.body, companyNewSchema);
         if (!validator.valid) {
@@ -61,7 +61,6 @@ router.get('/', async function (req, res, next) {
             req.query.maxEmployees
         ) {
             params = parseQueries(req.query);
-            console.log(params);
             const validator = jsonschema.validate(params, companyFilterSchema);
             if (!validator.valid) {
                 const errs = validator.errors.map((e) => e.stack);
@@ -105,33 +104,46 @@ router.get('/:handle', async function (req, res, next) {
  * Authorization required: login
  */
 
-router.patch('/:handle', ensureLoggedIn, async function (req, res, next) {
-    try {
-        const validator = jsonschema.validate(req.body, companyUpdateSchema);
-        if (!validator.valid) {
-            const errs = validator.errors.map((e) => e.stack);
-            throw new BadRequestError(errs);
-        }
+router.patch(
+    '/:handle',
+    ensureLoggedIn,
+    ensureAdmin,
+    async function (req, res, next) {
+        try {
+            const validator = jsonschema.validate(
+                req.body,
+                companyUpdateSchema
+            );
+            if (!validator.valid) {
+                const errs = validator.errors.map((e) => e.stack);
+                throw new BadRequestError(errs);
+            }
 
-        const company = await Company.update(req.params.handle, req.body);
-        return res.json({ company });
-    } catch (err) {
-        return next(err);
+            const company = await Company.update(req.params.handle, req.body);
+            return res.json({ company });
+        } catch (err) {
+            return next(err);
+        }
     }
-});
+);
 
 /** DELETE /[handle]  =>  { deleted: handle }
  *
  * Authorization: login
  */
 
-router.delete('/:handle', ensureLoggedIn, async function (req, res, next) {
-    try {
-        await Company.remove(req.params.handle);
-        return res.json({ deleted: req.params.handle });
-    } catch (err) {
-        return next(err);
+router.delete(
+    '/:handle',
+    ensureLoggedIn,
+    ensureAdmin,
+    async function (req, res, next) {
+        try {
+            await Company.remove(req.params.handle);
+            return res.json({ deleted: req.params.handle });
+        } catch (err) {
+            return next(err);
+        }
     }
-});
+);
 
 module.exports = router;
